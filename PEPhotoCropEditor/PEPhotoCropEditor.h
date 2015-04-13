9dcafccc93e184a4cb5194a5731afaaa1e0c57f4
visionhackerstudio <visionhackerstudio@gmail.com>
2015-04-13 22:16:58 +0800

    first commit

---
+++ PEPhotoCropEditor/PEPhotoCropEditor.h
@@ -0,0 +1,24 @@
+//
+//  PEPhotoCropEditor.h
+//  PEPhotoCropEditor
+//
+//  Created by 王淑羽 on 3/21/15.
+//  Copyright (c) 2015 Feather Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PEPhotoCropEditor.
+FOUNDATION_EXPORT double PEPhotoCropEditorVersionNumber;
+
+//! Project version string for PEPhotoCropEditor.
+FOUNDATION_EXPORT const unsigned char PEPhotoCropEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PEPhotoCropEditor/PublicHeader.h>
+
+
+#import <PEPhotoCropEditor/PECropViewController.h>
+#import <PEPhotoCropEditor/PECropView.h>
+#import <PEPhotoCropEditor/PECropRectView.h>
+#import <PEPhotoCropEditor/PEResizeControl.h>
+#import <PEPhotoCropEditor/UIImage+PECrop.h>
